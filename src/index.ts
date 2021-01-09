a231c7b8256358ccfb39196d47b2ce23a0de7b1f
Johannes Kortmann <johannes@kortm.net>
2021-01-09 16:21:57 +0100

    revert

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,10 @@
-import { ControlLoop, LocationOfInterest, Simulation } from 'rover'
-import { getSpeed, UnitSpeed } from './util/getSpeed'
-import LatLon from 'geodesy/latlon-spherical.js'
-import LatLonSpherical from 'geodesy/latlon-spherical'
-import { getEngineValues } from './util/getEngineValues'
-import { getHarmonicMean } from './util/getHarmonicMean'
-
-const LOIs: LocationOfInterest[] = [
-  {
-    latitude: 52.47880703639255,
-    longitude: 13.395281227289209,
-    label: 'A'
-  }
-]
-
-interface PriorMetrics {
-  location: LatLonSpherical | null
-  heading: number | null
-  clock: number | null
-  speeds: number[]
-}
-
-const priorMetrics: PriorMetrics = {
-  location: null,
-  heading: null,
-  clock: null,
-  speeds: [],
-}
-
-const updateMetrics = (location: LatLonSpherical, heading: number, clock: number) => {
-  priorMetrics.location = location
-  priorMetrics.heading = heading
-  priorMetrics.clock = clock
-}
-
-const updateSpeedMetrics = (currentSpeed: number) => {
-  if (priorMetrics.speeds.length > 10) {
-    priorMetrics.speeds.shift()
-  }
-  priorMetrics.speeds.push(currentSpeed)
-}
+import { ControlLoop, Simulation } from 'rover'
 
 const loop: ControlLoop = ({location, heading, clock}, {engines}) => {
-
-  const currentLocation = new LatLon(location.latitude, location.longitude)
-  const loiLocation = new LatLon(LOIs[0].latitude, LOIs[0].longitude)
-
-  const distanceToLoi = currentLocation.distanceTo(loiLocation)
-
-  if (priorMetrics.location && priorMetrics.clock) {
-    const speed = getSpeed(currentLocation, priorMetrics.location, clock, priorMetrics.clock, UnitSpeed.MetersPerSecond)
-    updateSpeedMetrics(speed)
-    updateMetrics(currentLocation, heading, clock)
-    const harmonicMeanSpeed = getHarmonicMean(priorMetrics.speeds)
-    return {
-      engines: getEngineValues(distanceToLoi, harmonicMeanSpeed)
-    }
-  } else {
-    updateMetrics(currentLocation, heading, clock)
-    return {
-      engines: [0, 0]
-    }
+  return {
+    engines: [0.6,0.6]
   }
 }
-
 const simulation = new Simulation({
   loop,
   origin: {
@@ -70,9 +12,11 @@ const simulation = new Simulation({
     longitude:13.395281227289209
   },
   element: document.querySelector('main') as HTMLElement,
-  locationsOfInterest: [
-    ...LOIs
-  ],
+  locationsOfInterest: [{
+    latitude: 52.47880703639255,
+    longitude: 13.395281227289209,
+    label: 'A'
+  }],
   renderingOptions: {
     width: 800,
     height: 800,
